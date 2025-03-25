a9fbf05792381e46b4a5f1e328261f834e5da86e
ysun209 <ysun209@aucklanduni.ac.nz>
2025-03-26 00:07:42 +1300

    fix: add / to branches

--- scripts/validate-branch.js
+++ scripts/validate-branch.js
@@ -6,16 +6,27 @@ const branchName = execSync('git symbolic-ref --short HEAD', {
 }).trim();
 
 // Define allowed prefixes
-const allowedPrefixes = ['feature', 'fix', 'hotfix', 'release'];
+const allowedPrefixes = [
+  'feature/',
+  'fix/',
+  'hotfix/',
+  'release/',
+  'chore/',
+  'test/',
+];
 
 // Check if the branch name matches the allowed prefixes
 const isValid = allowedPrefixes.some((prefix) => branchName.startsWith(prefix));
 
 if (!isValid) {
   console.error(
-    `ERROR: Branch name '${branchName}' does not start with 'feature/', 'fix/', 'hotfix/', or 'release/'`,
+    `ERROR: Branch name '${branchName}' does not start with 'feature/', 'fix/', 'hotfix/', 'chore/', 'test/', or 'release/'`,
   );
-  console.error('Please rename your branch to match one of these patterns.');
+  console.error(
+    'Please rename your branch to match one of these patterns using:',
+  );
+  console.error('git branch -m new-branch-name');
+  console.error('');
   process.exit(1); // Exit with an error code to prevent the push
 }
 
